76ed095e4aaf65b18b370cff6031b787c45a5c92
matu-gabriel <matu.dev25@gmail.com>
2024-06-12 22:38:53 -0300

    Criando rota inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./pages/Login";
 import GlobalStyles from "./styles/globalStyles";
 import { ToastContainer } from "react-toastify";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Login />
+  <>
+    <RouterProvider router={router} />
     <GlobalStyles />
     <ToastContainer autoClose={2000} theme="colored" />
-  </React.StrictMode>
+  </>
 );
