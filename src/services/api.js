6ff97a6f3aef32f49ced4c9203f2cae502a2464a
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-07-17 22:59:20 -0300

    Att rota base

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://devburger-api-final-production.up.railway.app/",
 });
 
 api.interceptors.request.use(async (config) => {
